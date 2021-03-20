fe197ab6acb7551a17d4617f2d9a7fbc8c5d1da3
handsome_sugar <anyonetff@msn.com>
2021-03-20 10:15:16 +0800

    impr(*): 增加了显示语言类型和文件大小的控件

---
+++ HentaiBlazor/Ezcomp/TextFileLength.razor.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HentaiBlazor.Ezcomp
+{
+    public partial class TextFileLength
+    {
+        private static long KB = 1024L;
+
+        private static long MB = 1024L * 1024L;
+
+        private static long GB = 1020L * 1024L * 1024L;
+
+        private static long TB = 1020L * 1024L * 1024L * 1024L;
+
+        [Parameter]
+        public long FileLength { get; set; }
+
+        private string _text()
+        {
+            if (FileLength < KB) 
+            {
+                return FileLength + " byte";
+            }
+            if (FileLength < MB)
+            {
+                return FileLength / KB + " KB";
+            }
+            if (FileLength < GB)
+            {
+                return FileLength / MB + " MB";
+            }
+            if (FileLength < TB)
+            {
+                return FileLength / GB + " GB";
+            }
+
+            return FileLength / TB + " TB";
+        }
+
+    }
+}
---
+++ HentaiBlazor/Ezcomp/TextLanguage.razor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HentaiBlazor.Ezcomp
+{
+    public partial class TextLanguage
+    {
+
+        [Parameter]
+        public string Language { get; set; }
+
+        private static Dictionary<string, string> _language;
+
+        static TextLanguage() 
+        {
+            _language = new Dictionary<string, string>();
+            _language.Add("ja", "日本語");
+            _language.Add("zh", "中文");
+            _language.Add("en", "English");
+        }
+
+        private string _text()
+        {
+            return _language.GetValueOrDefault(Language, "");
+        }
+
+    }
+}
