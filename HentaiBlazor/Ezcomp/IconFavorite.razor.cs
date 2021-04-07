673bbbc5575d352f6be5d5c60950394d25f29218
handsome_sugar <anyonetff@msn.com>
2021-04-07 18:09:53 +0800

    wip(favorite): 修改了搜藏控件.

---
+++ HentaiBlazor/Ezcomp/IconFavorite.razor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HentaiBlazor.Ezcomp
+{
+    public partial class IconFavorite
+    {
+        [Parameter]
+        public EventCallback<MouseEventArgs> OnClick { get; set; }
+
+        [Parameter]
+        public bool Favorite { get; set; }
+
+        private async Task HandleOnClick(MouseEventArgs args)
+        {
+            if (OnClick.HasDelegate)
+            {
+                await OnClick.InvokeAsync(args);
+            }
+        }
+
+    }
+}
